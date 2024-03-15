d26d439f4340adc5ff5ebc9506c4e751ae601ea4
muneebahmed502 <141343525+muneebahmed502@users.noreply.github.com>
2024-03-16 00:49:31 +0500

    nodejs

--- Basic-Node-Js/index.mjs
+++ Basic-Node-Js/index.mjs
@@ -3,6 +3,6 @@ import routes from './routes/index.mjs'
 const app = express()
 
 app.listen(3001, function (){
-    console.log('hello 3001 start')
+    console.log('hello 3000 start')
 })
 app.use("/",routes)
